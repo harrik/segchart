13972a5d50f8136b3a0c843c896261ab8dd61e76
harri_k <harri.kinnunen@hitsaamo.fi>
2013-04-08 01:44:32 +0300

    new dataset

--- sourcedata.js
+++ sourcedata.js
@@ -1,1 +1,13 @@
-var source_data = [10,15,30,70];
+var source_data = 
+[
+{Elain: "Kissa", Auto: "Audi", Koti: "Espoo", cases: 7},
+{Elain:"Koira", Auto: "Audi", Koti: "Espoo", cases:15},
+{Elain:"Koira", Auto: "BMW", Koti: "Espoo", cases: 20},
+{Elain:"Kani", Auto:"Hiace", Koti:"Espoo", cases: 13},
+{Elain:"Marsu", Auto:"Hiace", Koti:"Helsinki", cases:17},
+{Elain:"Koira", Auto:"BMW", Koti:"Helsinki", cases:8}
+
+]
+
+
+
